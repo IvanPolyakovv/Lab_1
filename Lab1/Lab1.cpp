f72219197c4018f7cd8bcac41561bc768e22f468
IvanPolyakovv <145684256+IvanPolyakovv@users.noreply.github.com>
2023-11-03 18:56:39 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "1.\n";
+	cout << "size of int: " << sizeof(int) << '\n' <<
+		"size of short int: " << sizeof(short int) << '\n' <<
+		"size of long int: " << sizeof(long int) << '\n' <<
+		"size of float: " << sizeof(float) << '\n' <<
+		"size of double: " << sizeof(double) << '\n' <<
+		"size of long double: " << sizeof(long double) << '\n' <<
+		"size of char: " << sizeof(char) << '\n' <<
+		"size of bool: " << sizeof(bool) << '\n';
+
+
+
+
+
+
+
+
+}
