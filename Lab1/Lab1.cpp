dd80ee85f2de86e4a1d15ea61c0c5c5e5b10b1bf
IvanPolyakovv <145684256+IvanPolyakovv@users.noreply.github.com>
2023-11-12 18:09:35 +0300

    Lab1.cpp

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
 	cout << "1.\n";
-	cout << "size of int: " << sizeof(int) << '\n' <<
-		"size of short int: " << sizeof(short int) << '\n' <<
-		"size of long int: " << sizeof(long int) << '\n' <<
-		"size of float: " << sizeof(float) << '\n' <<
-		"size of double: " << sizeof(double) << '\n' <<
-		"size of long double: " << sizeof(long double) << '\n' <<
-		"size of char: " << sizeof(char) << '\n' <<
-		"size of bool: " << sizeof(bool) << '\n';
+	cout << "size of int: " << sizeof(int) << '\n';
+	cout << "size of long int: " << sizeof(long int) << '\n';
+	cout << "size of float: " << sizeof(float) << '\n';
+	cout << "size of double: " << sizeof(double) << '\n';
+	cout << "size of long double: " << sizeof(long double) << '\n';
+	cout << "size of char: " << sizeof(char) << '\n';
+	cout << "size of bool: " << sizeof(bool) << '\n';
 
+	cout << "2.\n";
+	int number;
+	cout << "Enter the integer number: ";
+	cin >> number;
+	unsigned int order = 32;
+	unsigned int mask = 1 << order - 1;
+	for (int i = 1; i <= order; i++)
+	{
+		cout << (number & mask ? '1' : '0');
+		number = number << 1;
+		if (i % 8 == 0)
+		{
+			cout << ' ';
+		}
+		if (i % order - 1 == 0)
+		{
+			cout << ' ';
+		}
+	}
+	cout << '\n';
 
+	cout << "3.\n";
+	union
+	{
+		int intNumber;
+		float floatNumber;
+	};
+	cout << "Enter the float number: ";
+	cin >> floatNumber;
+	for (int i = 1; i <= order; i++)
+	{
+		cout << (intNumber & mask ? '1' : '0');
+		intNumber = intNumber << 1;
+		if (i % 8 == 0)
+		{
+			cout << ' ';
+		}
+		if (i % order - 1 == 0)
+		{
+			cout << ' ';
+		}
+	}
 
-
-
-
-
-
+	return 0;
 }
